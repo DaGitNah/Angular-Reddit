74931518fcfcb7d8e8b81b672cfd0959d3b0d074
Martijn Welker <martijn@blocklevel.nl>
2015-06-18 14:18:57 +0200

    Add link quickview, auto scrolltop, comment postdate

--- directives.js
+++ directives.js
@@ -1,27 +1,38 @@
 
-app.directive('viewContent', ['redditApiService' ,function() {
+app.directive('clickview', ['redditApiService', function(redditApiService) {
 	return {
-		restrict: 'A',
-		scope: {},
-		replace: false,
-		template: '<div ng-bind-html="content"></div>',
+		restrict: 'E',
+		scope: {
+			link: '='
+		},
+		template: 
+		'<div>'+
+			'<span ng-if="!html" class="btn-quickshow">View content</span>'+
+			'<div ng-if="html" ng-bind-html="html | markdown" class="quickshow"></div>'+
+		'</div>',
 		link: function (scope, element, attrs) {
-			element.on('click', function(){
-				redditApiService.getPost().success()
-				//scope.content = attrs.viewContent;
+			element.on('click', function(){	
+				redditApiService.getPost(scope.link.subreddit, scope.link.id, 1).success(function (data, status, headers, config){
+					scope.html = data[0].data.children[0].data.selftext;
+					console.log(scope.html)
+				}).error(function(data, status, headers, config) {
+					scope.html = 'An error occured, please check your request. Errorcode: '+status;
+
+				});
+				
 			})
 		}
 	};
 }])
 
 .directive("tree", function(RecursionHelper) {
-    return {
-        restrict: "E",
-        scope: {
-        	comment: '='
-        },
-        templateUrl: 'templates/comments.html',
-        compile: function(element) {
+	return {
+		restrict: "E",
+		scope: {
+			comment: '='
+		},
+		templateUrl: 'templates/comments.html',
+		compile: function(element) {
             // Use the compile function from the RecursionHelper,
             // And return the linking function(s) which it returns
             return RecursionHelper.compile(element);
